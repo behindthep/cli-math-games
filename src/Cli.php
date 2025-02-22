6608f1d0c4ef4479ef7f97c705ea438e9d040d8f
Alexey Kolvakh <alex.kolvah@mail.ru>
2025-02-22 12:15:28 +0300

    add prompt function doc in Cli.php

--- src/Cli.php
+++ src/Cli.php
@@ -5,6 +5,11 @@ namespace Brain\Games\Cli;
 use function cli\line;
 use function cli\prompt;
 
+/**
+* cli\prompt signature:
+* cli\prompt($question, $default = false, $marker = ':')
+* https://github.com/wp-cli/php-cli-tools?tab=readme-ov-file#function-list
+*/
 function run(): void
 {
     line("Welcome to the Brain Game!");
